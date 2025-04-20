14e72dcf2abb5bd339a19aec2620bdf07bd6216d
xFinVi <111379135+xFinVi@users.noreply.github.com>
2025-04-20 16:16:40 +0100

    Add files via upload

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -196,14 +196,14 @@ function CarDetails() {
           {/* Details Section */}
           <motion.div
             variants={itemVariants}
-            className="flex flex-col overflow-scroll h-[85vh] bg-white  shadow-md rounded-b-xl"
+            className="flex flex-col h-[85vh] bg-white  shadow-md rounded-b-xl"
           >
             <motion.div
               variants={itemVariants}
-              className="w-full mx-auto bg-center bg-cover shadow-lg min-h-80 lg:h-80 lg:bg-no-repeat"
+              className="w-full mx-auto bg-center bg-cover shadow-lg xl:bg-contain min-h-80 lg:h-80 lg:bg-no-repeat"
               style={{ backgroundImage: `url(${imageUrl})` }}
             ></motion.div>
-            <div className=" sm:w-[80%] lg:w-3/4 mt-2  mx-auto grid items-start grid-cols-1 gap-10 p-2 sm:p-8 md:gap-6 sm:grid-cols-1 lg:grid-cols-2 place-items-center">
+            <div className=" sm:w-[80%] lg:w-3/4   mx-auto grid items-start grid-cols-1 gap-10 p-2 sm:p-4 md:gap-6 sm:grid-cols-1 lg:grid-cols-2 place-items-center">
               {/* Background Image Section */}
 
               {/* General Section */}
