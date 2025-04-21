24be8d7558b16a1d9ab68942a204f3d9045622f6
Filippos <philterzidis@hotmail.com>
2025-04-21 17:31:04 +0100

    updated styling

--- src/components/vehicleCard.jsx
+++ src/components/vehicleCard.jsx
@@ -34,7 +34,7 @@ function VehicleCard({
         onClick={handleImageClick}
       />
 
-      <div className="flex items-center justify-between px-3 py-2">
+      <div className="flex items-center justify-between px-3 py-1">
         <div className="">
           <h3 className="text-base font-semibold text-gray-800">
             {vehicle.registrationNumber}
